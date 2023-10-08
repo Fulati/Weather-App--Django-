8d81d9fa0bfb1fc15b38bc4cb71be349fd3b2999
Fulati Aizihaer <azhar.pulat@gmail.com>
2023-10-07 21:54:22 -0700

    fixed

--- the_weather/wsgi.py
+++ the_weather/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_weather.settings')
 
 application = get_wsgi_application()
-
-app = application
